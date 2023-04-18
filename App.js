d82a36808b84516505f6236001f0b9310f50afec
jvsobraz <jvsobraz@hotmail.com>
2023-04-18 08:32:13 -0300

    Implement dos diretórios App.js e ThemeContext.js

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+
+import { ThemeContext, themes } from "./context/ThemeContext";
+import Header from "./components/Header";
+import ThemeToggler from "./components/ThemeToggler";
 
 export default function App() {
+  const [theme, setTheme] = useState(themes.light);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeContext.Provider value={[theme, setTheme]}>
+      <Header/>
+      <ThemeToggler/>
+    </ThemeContext.Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
